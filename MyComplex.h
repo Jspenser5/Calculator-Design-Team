210cc0d51453d6d404f3f028707d7cd29026ffd3
Jspenser5 <95122795+Jspenser5@users.noreply.github.com>
2022-05-09 22:31:27 +0300

    Почти завершённый класс Комплексных чисел

---
+++ MyComplex.h
@@ -0,0 +1,27 @@
+#ifndef MY_COMPLEX_H
+#define MY_COMPLEX_H
+#include<string>
+#include<iostream>
+using namespace std;
+//Функция возведения в степень 
+double power(double a, int p);
+// Извлекает из строки число типа double но не удаляет , а возвращает индекс конца в переменную id
+//Вспомогательная для extract(string& str) класса complex
+double extract_double_num(const string& str, int& idx);
+class complex {
+public:
+	double Re;
+	double Im;
+	//Функция ручной установки значений
+	void set(double R, double I);
+	//Функция печати на консоль
+	void print();
+	//Функция извлекает комплекс. из начала строки и удаляет в случае успеха , иначе кидает exception
+	void extract(string& str);
+	complex operator +(complex c2);
+	complex operator -(complex c2);
+	complex operator *(complex c2);
+	complex operator /(complex c2);
+	complex operator =(complex cm);
+};
+#endif
