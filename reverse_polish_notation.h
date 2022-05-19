898728720f5ba0cc0d3fe6510e67ee219a2160f1
Jspenser5 <95122795+Jspenser5@users.noreply.github.com>
2022-05-19 23:39:45 +0300

    Изменена функция reverse_polish_notation

--- reverse_polish_notation.h
+++ reverse_polish_notation.h
@@ -6,5 +6,5 @@ using namespace std;
 //Функция для проверки наличия Данного элемента в векторе символов
 int in_vector(vector<char> v, char value);
 //Функция для приведения выражения по ОПЗ в соотвецтвии с приоритетами операций
-string reverse_polish_notation(string& expression, vector<char>& priorities);
+string reverse_polish_notation(string& expression, vector<char>& operators, vector<int> priorities);
 #endif
