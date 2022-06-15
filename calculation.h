e5b0f597dad3bd65e38c3ea2e4581be40bb0da35
Evgesha1997 <95122839+Evgesha1997@users.noreply.github.com>
2022-06-15 19:37:39 +0300

    Add files via upload

---
+++ calculation.h
@@ -0,0 +1,15 @@
+#ifndef _CALCULATION_
+#define _CALCULATION_
+#include<string>
+#include"Complex.h"
+#include "Rational.h"
+// длин арифм
+#include"Stack.h"
+
+void rational_calc(string& OPZ);
+
+void complex_calc(string& OPZ);
+
+void long_calc(string& OPZ);
+
+#endif // _CALCULATION_
